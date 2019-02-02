2ee1b5bcfd0876fc767a9b6b01a20e5be9e63e3d
kuroganechong <kuroganechong@gmail.com>
2019-02-02 22:47:05 +0800

    Added webpack for html/js excluding node-modules

---
+++ templates/templates/src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../assets/js/bootstrap.min.js'
+import '../assets/css/bootstrap.min.css'
+import '../assets/css/starter-template.css'
+/* eslint-disable-next-line no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
